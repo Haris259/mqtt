b2d688a1fe8425f9ff9b41ff8464763cdc4db12b
haris3099 <haris.saleem@septemsystems.com>
2024-06-28 15:35:31 +0500

    change readable array

--- android/src/main/java/com/tuanpm/RCTMqtt/RCTMqttModule.java
+++ android/src/main/java/com/tuanpm/RCTMqtt/RCTMqttModule.java
@@ -12,6 +12,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableArray;
 
 import java.util.HashMap;
 import java.util.UUID;
