967c60679fe29a5e1a914dd864c23aacfe0605a2
haris3099 <haris.saleem@septemsystems.com>
2024-06-28 19:51:25 +0500

    fix

--- android/src/main/java/com/tuanpm/RCTMqtt/RCTMqtt.java
+++ android/src/main/java/com/tuanpm/RCTMqtt/RCTMqtt.java
@@ -467,6 +467,20 @@ public void publishBuffer(@NonNull final String topic, @NonNull final ReadableAr
     public void messageArrived(@NonNull final String topic, @NonNull final MqttMessage message) throws MqttException {
         // Called when a message arrives from the server that matches any
         // subscription made by the client
+        // Called when a message arrives from the server that matches any subscription made by the client
+    String logMessage = new StringBuilder("  Topic:\t")
+            .append(topic)
+            .append("  Message:\t")
+            .append(new String(message.getPayload()))
+            .append("  QoS:\t")
+            .append(message.getQos())
+            .toString();
+
+    // Log the message
+    Log.d("MQTT>>", logMessage);
+
+
+
 
         log(new StringBuilder("  Topic:\t").append(topic).append("  Message:\t")
                 .append(new String(message.getPayload())).append("  QoS:\t").append(message.getQos()).toString());
